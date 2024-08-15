ed2d821653dd98c2fb51cecc58c4105cc1139094
Gabriel Barros <barros.fontes@unesp.br>
2024-08-14 21:59:26 -0300

    add mongo and docker integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,41 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import { UserResolver } from './user/user.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
 
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const mongoUser = configService.get<string>('MONGO_USERNAME');
+        const mongoPassword = configService.get<string>('MONGO_PASSWORD');
+        const mongoHost = configService.get<string>('MONGO_HOST');
+        const mongoPort = configService.get<string>('MONGO_PORT');
+        const mongoDB = configService.get<string>('MONGO_DB');
+
+        return {
+          uri: `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDB}?authSource=admin`,
+        };
+      },
+      inject: [ConfigService],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
       playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()]
-    })
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, UserResolver],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000,() => console.log('Running on localhost:3000')
-  );
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+
+  const serverUrl = await app.getUrl();
+
+  const url = serverUrl.replace('[::1]', 'localhost');
+
+  Logger.log(`🚀 Server is running on: ${url}`, 'API');
 }
 bootstrap();
