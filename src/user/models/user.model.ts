ffc7083878965941806824c74476a54bc5b85957
Gabriel Barros <barros.fontes@unesp.br>
2024-08-14 22:19:32 -0300

    create user mongoose model

--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -1,17 +1,23 @@
-import { Field, ID, ObjectType } from "@nestjs/graphql";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
+@Schema({ timestamps: true })
 @ObjectType()
 export class User {
   @Field(() => ID)
   id: string;
 
   @Field()
+  @Prop({ required: true })
   name: string;
 
   @Field()
+  @Prop({ required: true })
   doc: string;
 
   @Field()
+  @Prop({ required: true })
   docType: string;
 
   @Field(() => Date)
@@ -20,3 +26,6 @@ export class User {
   @Field(() => Date)
   updatedAt: Date;
 }
+
+export type UserDocument = HydratedDocument<User>;
+export const UserSchema = SchemaFactory.createForClass(User);
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserResolver } from './user.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './models/user.model';
 
 @Module({
-  providers: [UserResolver]
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UserResolver],
 })
 export class UserModule {}
